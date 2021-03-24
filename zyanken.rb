ab1f9ed7bdc01ffee89c6a476f917bfcc95b285f
EC2 Default User <ec2-user@ip-172-31-33-227.ap-northeast-1.compute.internal>
2021-03-24 13:05:43 +0000

    f

---
+++ zyanken.rb
@@ -0,0 +1,48 @@
+
+while true do
+	puts "じゃんけん..."
+	puts "0(グー)1(チョキ)2(パー)3(戦わない)"
+	
+	player_hand = gets.to_i
+	computer_hand = rand(3)
+	
+	unless player_hand == 3
+	puts "あなた：#{player_hand}を出しました"
+	puts "相手：#{computer_hand}を出しました"
+
+	while player_hand == computer_hand do
+	puts "あいこで"
+	puts "0(グー)1(チョキ)2(パー)3(戦わない)"
+	
+	player_hand = gets.to_i
+	computer_hand = rand(3)
+	
+	puts "あなた：#{player_hand}を出しました"
+	puts "相手：#{computer_hand}を出しました"
+
+	end
+	
+	if player_hand == 0 && computer_hand == 1 || player_hand == 1 && computer_hand == 2 || player_hand == 2 && computer_hand == 0
+	player_winner_flag = true
+	else
+	player_winner_flag = false
+	end
+	
+	puts "あっち向いて〜"
+	puts "0(上)1(下)2(左)3(右)"
+	player_direction = gets.to_i
+	computer_direction = rand(4)
+	puts "ホイ！"
+	puts "---------------"
+	puts "あなた：#{player_direction}"
+	puts "相手：#{computer_direction}"
+	
+	if player_winner_flag && player_direction == computer_direction
+	puts "あなたの勝ちです!"
+	elsif !player_winner_flag && player_direction == computer_direction
+	puts "CPの勝ちです!"
+	end
+	else
+	break
+	end
+	end
